1705857f6e93929e3cd92f0ee3e07bc4ac8aded2
João Victor Ferreira Borba <60452036+J-Borba@users.noreply.github.com>
2024-01-28 20:20:28 -0300

    Task 015 [28/01/2024 - 20:20] - Adding Entity Certificate

--- frontend/src/1-Curriculo/views/CertificadosView.vue
+++ frontend/src/1-Curriculo/views/CertificadosView.vue
@@ -3,13 +3,13 @@
     <div>
       <h1 class="h3 mb-5 text-center">Certificados</h1>
       <div class="d-flex flex-column gap-5 border border-secondary rounded-2 p-3 bg-emphasis">
-        <div class="d-flex flex-column gap-3" v-for="company in companies" :key="company.id">
+        <div class="d-flex flex-column gap-3" v-for="(company, index) in companies" :key="index">
           <div class="d-flex align-items-center gap-2">
             <img :src="company.icon" alt="company icon" />
             <p class="h5">{{ company.name }}</p>
           </div>
           <div class="d-flex flex-column gap-2 ms-2">
-            <div v-for="certificate in company.certificates" :key="certificate.id">
+            <div v-for="(certificate, index) in company.certificates" :key="index">
               <a v-if="certificate.title && certificate.url" class="secondary" :href="certificate.url" target="_blank">
                 <font-awesome-icon :icon="['fas', 'medal']" /> {{ certificate.title }}
               </a>
@@ -28,39 +28,36 @@
 
   const companies = [
     {
-      id: 2,
       name: 'Alura',
       icon: aluraIcon,
       certificates: [
         {
-          id: 2,
+          title: '.NET 6: relacionando entidades',
+          url: 'https://cursos.alura.com.br/certificate/310b7b5b-eff5-4114-b888-2d938d9b15f0',
+        },
+        {
           title: '.NET 6: criando uma web API',
           url: 'https://cursos.alura.com.br/certificate/0a73ce25-571d-4d5d-8d42-b84a57123a78?lang=pt_BR',
         },
         {
-          id: 1,
           title: 'HTTP: entendendo a web por baixo dos panos',
           url: 'https://cursos.alura.com.br/certificate/6251e3fc-d35f-4e7c-9c30-b5d1ab07db59',
         },
       ],
     },
     {
-      id: 1,
       name: 'Udemy',
       icon: udemyIcon,
       certificates: [
         {
-          id: 3,
           title: 'C# COMPLETO Programação Orientada a Objetos + Projetos',
           url: 'https://www.udemy.com/certificate/UC-855c52b9-7d99-4268-814a-b518bef8c295/',
         },
         {
-          id: 2,
           title: 'The Complete 2023 Web Development Bootcamp',
           url: 'https://www.udemy.com/certificate/UC-3e28fcf8-faed-4241-9940-002f7c1afb63/',
         },
         {
-          id: 1,
           title: 'Power BI Completo - Do Básico ao Avançado',
           url: 'https://www.udemy.com/certificate/UC-d2e1559c-5b7a-45b7-9168-e1db9a8aa03d/',
         },
