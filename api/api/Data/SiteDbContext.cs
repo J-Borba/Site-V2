df5c08f91e66c729e2ffb61c5f5edff62d7c4a52
João Victor Ferreira Borba <60452036+J-Borba@users.noreply.github.com>
2024-02-05 23:49:14 -0300

    Task 018 [05/02/2024 - 23:49] - api first steps

---
+++ api/api/Data/SiteDbContext.cs
@@ -0,0 +1,10 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class SiteDbContext : IdentityDbContext<User>
+{
+    public SiteDbContext(DbContextOptions options) : base(options) { }
+}
---
+++ api/api/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using api.Models;
+
+namespace api.Services.Interfaces;
+
+public interface ITokenService
+{
+    public string GetToken(User user);
+}
