55fab8996f7b5f0341ccd84ecc4a184077c238ed
João Borba <jvfborba+github@gmail.com>
2026-04-18 22:10:24 -0300

    bugfix: Adjusting router scroll to top

--- frontend/src/0-Global/router/index.ts
+++ frontend/src/0-Global/router/index.ts
@@ -20,6 +20,9 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
   history: createWebHistory(),
   routes,
 });
